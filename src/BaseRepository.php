bf4b9eba4647d6805628cdc70844af996a0ede2d
Jamie Sykes <jaymeh@users.noreply.github.com>
2023-12-19 11:53:48 +0000

    Initial Module Implementation.

---
+++ src/BaseRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Creode\LaravelRepostory;
+
+abstract class BaseRepository {
+    /**
+     * The model instance.
+     *
+     * @var \Illuminate\Database\Eloquent\Model
+     */
+    protected $model;
+
+    public function __construct()
+    {
+        $modelClass = $this->getModel();
+        $this->model = new $modelClass;
+    }
+
+    /**
+     * Gets the model class name.
+     *
+     * @return string
+     */
+    abstract function getModel(): string;
+
+    /**
+     * PHP Magic method which will pass any method calls to the model.
+     *
+     * @param string $method
+     * @param mixed $arguments
+     * @return mixed
+     */
+    public function __call($method, $arguments)
+    {
+        // Check if the method exists in the current class
+        if (method_exists($this, $method)) {
+            return call_user_func_array([$this, $method], $arguments);
+        }
+
+        $modelClassName = $this->getModel();
+
+        // Check if the method exists in the model class
+        // and if it's static, it will be called statically
+        if (method_exists($modelClassName, $method)) {
+            return call_user_func_array([$modelClassName, $method], $arguments);
+        }
+
+        // If the method is non-static, we would need to instantiate the model
+        if (is_callable([$modelClassName, $method])) {
+            $modelInstance = new $modelClassName();
+            return call_user_func_array([$modelInstance, $method], $arguments);
+        }
+
+        throw new \BadMethodCallException("Method {$method} does not exist on " . get_class($this) . " or " . $modelClassName);
+    }
+}
