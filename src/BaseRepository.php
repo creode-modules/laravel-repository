9d8a9db09d9587d069da8ad3242972b0687d9ae9
Jamie Sykes <jaymeh@users.noreply.github.com>
2024-01-05 16:45:54 +0000

    Removes redundant code as it is already handled by the callStatic function.

--- src/BaseRepository.php
+++ src/BaseRepository.php
@@ -32,12 +32,6 @@ abstract class BaseRepository
 
         $modelClassName = $this->getModel();
 
-        // Check if the method exists in the model class
-        // and if it's static, it will be called statically
-        if (method_exists($modelClassName, $method)) {
-            return call_user_func_array([$modelClassName, $method], $arguments);
-        }
-
         // If the method is non-static, we would need to instantiate the model
         if (is_callable([$modelClassName, $method])) {
             $modelInstance = new $modelClassName();
